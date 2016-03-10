90cf3c3496be5df139db39c349f85c0c156c002f
Eben Zhang <ebenz@embedcard.com>
2016-03-10 13:10:50 +0800

    Hard reset the branch to target and confirm before doing it.

--- FetchAndCheckoutBranch.py
+++ FetchAndCheckoutBranch.py
@@ -7,11 +7,17 @@ def fetch_and_check_out(branch_name):
     print cmd
     os.system(cmd)
 
+    result = os.popen('git status -s --untracked-files=no').read()
+    if result != "":
+        print result
+        choice = str(raw_input("You have uncommitted local changes. Press 'Y' to continue."))
+        if not choice.upper().startswith('Y'):
+            return
     cmd = "git checkout " + branch_name
     print cmd
     os.system(cmd)
 
-    cmd = "git reset origin/" + branch_name
+    cmd = "git reset --hard origin/" + branch_name
     print cmd
     os.system(cmd)
 
