9fc12c97b6944cac98ee8a1040759ddd5a3f9e66
Eben Zhang <ebenz@embedcard.com>
2016-03-29 17:09:27 +0800

    Able to checkout a branch based on remote branch

--- FetchAndCheckoutBranch.py
+++ FetchAndCheckoutBranch.py
@@ -2,8 +2,8 @@ import sys
 import os
 
 
-def fetch_and_check_out(branch_name):
-    cmd = "git fetch origin " + branch_name
+def fetch_and_check_out(remote_branch_name, new_branch_name):
+    cmd = "git fetch origin " + remote_branch_name
     print cmd
     os.system(cmd)
 
@@ -13,17 +13,24 @@ def fetch_and_check_out(branch_name):
         choice = str(raw_input("You have uncommitted local changes. Press 'Y' to continue."))
         if not choice.upper().startswith('Y'):
             return
-    cmd = "git checkout " + branch_name
+
+    cmd = "git branch -D " + new_branch_name
     print cmd
     os.system(cmd)
 
-    cmd = "git reset --hard origin/" + branch_name
+    cmd = "git checkout -f -b " + new_branch_name + " origin/" + remote_branch_name
+    print cmd
+    os.system(cmd)
+
+    cmd = "git reset --hard origin/" + remote_branch_name
     print cmd
     os.system(cmd)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print "branch name required"
-    else:
-        fetch_and_check_out(sys.argv[1])
+    elif len(sys.argv) == 2:
+        fetch_and_check_out(sys.argv[1], sys.argv[1])
+    elif len(sys.argv) == 3:
+        fetch_and_check_out(sys.argv[1], sys.argv[2])
 
