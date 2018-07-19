16e5090fd48404c5b457c74017518f48e457aa96
Eben Zhang <ebenz@embedcard.com>
2018-07-19 15:02:15 +0800

    Extract git related script to separate file

--- powershellprofile.ps1
+++ powershellprofile.ps1
@@ -38,38 +38,7 @@ function j([string]$jiraIssue) {
     & start "https://helixleisure.atlassian.net/browse/$jiraIssue" 
 }
 
-# git fetch a branch
-function gf([string]$branch) {
-    Write-Host "fetching $branch"
-    & git fetch origin $branch
-    if($LASTEXITCODE -ne 0) {
-        throw "Failed to fetch branch $branch"
-    }
-}
-
-# git fetch and checkout branch
-function gfc([string]$branch) {
-    gf -branch $branch
-
-    $uncommited = & git status -s --untracked-files=no
-    if(![string]::IsNullOrWhiteSpace($uncommited)) {
-        $choice = Read-Host -Prompt "You have uncommitted local changes. Press 'Y' to continue."
-
-        if($choice -ne "Y") {
-            return
-        }
-
-    }
-
-    $curBranch = & git rev-parse --abbrev-ref HEAD
-    if($curBranch -eq $branch) {
-        & git reset --hard origin/$branch
-    } else {
-        & git branch -D $branch
-        
-        & git checkout -f -b $branch "origin/$branch"
-    }
-}
+. $PSScriptRoot/git.ps1
 
 function msbuild() {
     msbuild.exe "/t:restore;build" /m $args
